2c6c4dbb561a24d286c09f3335fb63318400bc10
aneekaxx <102327938+aneekaxx@users.noreply.github.com>
2022-03-24 21:16:38 +0000

    Add files via upload

---
+++ src/cycling/Segment.java
@@ -0,0 +1,75 @@
+package cycling;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+import java.util.HashMap;
+
+public class Segment implements Serializable{
+	
+	SegmentType segmentType;
+	double segmentLocation;
+	double segmentLength;
+	double avgGradient;
+	
+	HashMap<Integer, LocalTime> riderTimes =  new HashMap<Integer, LocalTime>();
+	
+	public Segment(double loc, SegmentType type, double aGrad, double length){
+		segmentLocation = loc;
+		segmentType = type;
+		avgGradient = aGrad;
+		segmentLength = length;
+		System.out.println("Segment has been constructed. " + this);
+	}
+	
+		//Type
+		public SegmentType getSegmentType() {
+			return segmentType;
+		}
+		
+		public SegmentType setSegmentType(SegmentType type) {
+			return segmentType = type;
+		}
+		
+		
+		//Description
+		public double getSegmentLocation() {
+			return segmentLocation;
+		}
+		
+		public void setSegmentLocation(double location) {
+			segmentLocation = location;
+		}
+		
+		
+		//Length
+		public double getSegmentLength() {
+			return segmentLength;
+		}
+		
+		public void setSegmentLength(double length) {
+			segmentLength = length;
+		}
+		
+		
+		
+		public double getAverageGradient() {
+			return avgGradient;
+		}
+		
+		public void setAverageGradient(double aGrad) {
+			avgGradient = aGrad;
+		}
+		
+		//Time
+		public void setRiderTime(int riderId, LocalTime time) {
+			riderTimes.put(riderId, time);
+		}
+		
+		
+		//toString
+		@Override
+		public String toString() {
+			String str = String.format("Type: %s, Location: %f, Length: %f, AverageGradient: %f", segmentType, segmentLocation, segmentLength, avgGradient);
+			return str;
+		}
+}
